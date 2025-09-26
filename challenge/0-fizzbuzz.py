cfcaa0bea82cf40b2e28768a0f1caf654d419f02
Mr Philips <ddoudou7@yahoo.fr>
2025-09-26 06:54:43 +0200

    fix(0-fizzbuzz): check 15 first (FizzBuzz), exact spacing & CLI

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,39 +1,39 @@
 #!/usr/bin/python3
-""" FizzBuzz
 """
+FizzBuzz: print numbers from 1 to n separated by a single space.
+- Multiples of 3     -> "Fizz"
+- Multiples of 5     -> "Buzz"
+- Multiples of 3 & 5 -> "FizzBuzz"
+"""
+
 import sys
 
 
-def fizzbuzz(n):
-    """
-    FizzBuzz function prints numbers from 1 to n separated by a space.
-
-    - For multiples of three print "Fizz".
-    - For multiples of five print "Buzz".
-    - For numbers which are multiples of both three and five print "FizzBuzz".
-    """
+def fizzbuzz(n: int) -> None:
     if n < 1:
         return
-
-    tmp_result = []
+    out = []
     for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
-        elif (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 5) == 0:
-            tmp_result.append("Buzz")
+        if i % 15 == 0:
+            out.append("FizzBuzz")
+        elif i % 3 == 0:
+            out.append("Fizz")
+        elif i % 5 == 0:
+            out.append("Buzz")
         else:
-            tmp_result.append(str(i))
-    print(" ".join(tmp_result))
+            out.append(str(i))
+    print(" ".join(out))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) <= 1:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
-
-    number = int(sys.argv[1])
+    try:
+        number = int(sys.argv[1])
+    except ValueError:
+        print("Invalid number")
+        sys.exit(1)
     fizzbuzz(number)
