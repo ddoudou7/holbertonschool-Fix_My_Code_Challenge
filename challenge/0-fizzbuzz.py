e807f7ef5b56f099d12c5719ee706551ef09ab94
Mr Philips <ddoudou7@yahoo.fr>
2025-09-27 15:14:42 +0200

    Rewrite clean FizzBuzz solution: handle 15 first, exact output

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -5,19 +5,22 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: ./0-fizzbuzz.py <number>")
         sys.exit(1)
+
     try:
         n = int(sys.argv[1])
     except ValueError:
         print("Usage: ./0-fizzbuzz.py <number>")
         sys.exit(1)
 
+    result = []
     for i in range(1, n + 1):
         if i % 15 == 0:
-            print("FizzBuzz", end=" ")
+            result.append("FizzBuzz")
         elif i % 3 == 0:
-            print("Fizz", end=" ")
+            result.append("Fizz")
         elif i % 5 == 0:
-            print("Buzz", end=" ")
+            result.append("Buzz")
         else:
-            print(i, end=" ")
-    print()
+            result.append(str(i))
+
+    print(" ".join(result))
